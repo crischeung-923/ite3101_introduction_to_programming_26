34f2d653b823a9358d579c511b9e0a8531548f17
crischeung-923 <crischeung923@gmail.com>
2026-09-04 08:24:40 +0000

    [skip actions] lab/lab01/ch01_t07_updating_variables.py

--- lab/lab01/ch01_t07_updating_variables.py
+++ lab/lab01/ch01_t07_updating_variables.py
@@ -9,6 +9,8 @@ annual_rainfall += august_rainfall
 
 september_rainfall = 5.16
 annual_rainfall += september_rainfall
+
 october_rainfall = 7.20
+annual_rainfall += 
 november_rainfall = 5.06
 december_rainfall = 4.06
