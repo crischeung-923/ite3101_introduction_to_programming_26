5a7ce608896e9cff3a25f99c94e941f513abdc5c
crischeung-923 <crischeung923@gmail.com>
2026-09-08 02:03:17 +0000

    [skip actions] lab/lab01/ch01_t04_handling_errors.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-API_KEY = ""
-API_ENDPOINT = ''
+API_KEY = "AIzaSyAjvOXa6ylu0PPLNesFc1WmYOdg9EmtJho"
+API_ENDPOINT = 'https://gateway-7cyi7dui.ue.gateway.dev'
--- lab/lab01/ch01_t04_handling_errors.py
+++ lab/lab01/ch01_t04_handling_errors.py
@@ -1,4 +1,3 @@
-# print("How do you make a hot dog stand?')
-# print(You take away its chair!)
+
 print("How do you make a hot dog stand")
 print('You take away its chair!')
