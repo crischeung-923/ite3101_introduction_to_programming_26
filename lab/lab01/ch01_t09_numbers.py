798878fd94703d7d3ffa0b26d76bd8f3bc8b5762
crischeung-923 <crischeung923@gmail.com>
2026-09-08 02:48:53 +0000

    [skip actions] lab/lab01/ch01_t09_numbers.py

--- lab/lab01/ch01_t09_numbers.py
+++ lab/lab01/ch01_t09_numbers.py
@@ -1,4 +1,4 @@
-cucumbers=1
-price_per_cucumber=3.25
-total_cos=cucumbers * price_per_cucumber
-ptint(total_cos)
+cucumbers = 1
+price_per_cucumber = 3.25
+total_cos = cucumbers * price_per_cucumber
+print(total_cos)
