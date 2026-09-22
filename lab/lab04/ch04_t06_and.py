6cebdbe21c9b510c08c25d95f65416a0dd65ea09
crischeung-923 <crischeung923@gmail.com>
2026-09-22 03:11:23 +0000

    [skip actions] lab/lab04/ch04_t06_and.py

--- lab/lab04/ch04_t06_and.py
+++ lab/lab04/ch04_t06_and.py
@@ -1,9 +1,9 @@
-bool_one = None
-False and False
-bool_two = None
--(-(-(-2))) == -2 and 4 >= 16 ** 0.5
-bool_three = None
+bool_one = 
 
-bool_four = None
+bool_two = -(-(-(-2))) == -2 and 4 >= 16 ** 0.5
 
-bool_five = None
+bool_three = 
+
+bool_four = 
+
+bool_five = 
